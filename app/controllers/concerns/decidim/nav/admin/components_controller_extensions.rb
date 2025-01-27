60910a8baa3e6e21a42a779fee18735995d119e0
Joonas <joonas.aapro@mainiotech.fi>
2025-01-27 10:16:47 +0200

    Fix icon link to extension

--- app/controllers/concerns/decidim/nav/admin/components_controller_extensions.rb
+++ app/controllers/concerns/decidim/nav/admin/components_controller_extensions.rb
@@ -15,16 +15,16 @@ module Decidim
 
           alias_method :icon_link_to_orig, :icon_link_to unless private_method_defined?(:icon_link_to_orig)
 
-          delegate :link_to, :content_tag, :_icon_classes, :asset_pack_path, to: :helpers
+          delegate :link_to, :content_tag, :_icon_classes, :asset_pack_path, :with_tooltip, :icon, to: :helpers
 
           def icon_link_to(icon_name, url, ...)
             extra = nil
-            if icon_name == "cog" && respond_to?(:component_links_path)
+            if icon_name == "settings-4-line" && respond_to?(:component_links_path)
               # Add the link to the links management after the cog icon
               # extra = icon_link_to_orig()
               match = url.match(%r{/([0-9]+)/edit(\?.*)?$})
               extra = icon_link_to_orig(
-                "globe",
+                "globe-line",
                 component_links_path(component_id: match[1]),
                 t("actions.links", scope: "decidim.admin"),
                 class: "action-icon--links"
