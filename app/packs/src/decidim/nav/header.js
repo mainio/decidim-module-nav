3be6ab0f1a9a4bf8c025718a5c741bce6cd03f56
Joonas <joonas.aapro@mainiotech.fi>
2025-04-17 08:00:21 +0300

    Add active links for main links and locales / Move locales to header / Remove extra header links

--- app/packs/src/decidim/nav/header.js
+++ app/packs/src/decidim/nav/header.js
@@ -4,12 +4,28 @@ const activeLink = () => {
   links.forEach(link => {
     link.classList.add("menu-link");
 
-    if (window.location.pathname === link.getAttribute("href")) {
+    if (window.location.pathname.includes(link.getAttribute("href"))) {
       link.classList.add("active-link");
     }
   });
 }
 
+const activeLocale = () => {
+  const localesContainer = document.querySelector(".simple-locales-container");
+
+  if (localesContainer) {
+    const locales = localesContainer.querySelectorAll(".locale");
+    const docLanguage = document.documentElement.lang;
+
+    locales.forEach(locale => {
+      if (docLanguage === locale.getAttribute("lang")) {
+        locale.classList.add("active-locale");
+      }
+    })
+  }
+}
+
 $(() => {
   activeLink();
+  activeLocale();
 })
