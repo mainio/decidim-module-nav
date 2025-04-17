80a3242db157f17633e43e3c2b7926822765cb30
Joonas <joonas.aapro@mainiotech.fi>
2025-04-17 09:29:43 +0300

    Change home menu logic

--- lib/decidim/nav/engine.rb
+++ lib/decidim/nav/engine.rb
@@ -8,6 +8,11 @@ module Decidim
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::Nav
 
+      initializer "decidim_nav.add_cells_view_paths", before: "decidim_core.add_cells_view_paths" do
+        Cell::ViewModel.view_paths << File.expand_path("#{Decidim::Nav::Engine.root}/app/cells")
+        Cell::ViewModel.view_paths << File.expand_path("#{Decidim::Nav::Engine.root}/app/views")
+      end
+
       initializer "decidim_nav.menu" do
         config.after_initialize do
           builder = Decidim::Nav::MenuBuilder.new
@@ -20,6 +25,9 @@ module Decidim
           # Presenter extensions
           Decidim::MenuPresenter.include(Decidim::Nav::MenuPresenterExtensions)
           Decidim::MenuItemPresenter.include(Decidim::Nav::MenuItemPresenterExtensions)
+
+          # Cell extensions
+          Decidim::ContentBlocks::GlobalMenuCell.include(Cell::ViewModel::Partial)
         end
       end
     end
