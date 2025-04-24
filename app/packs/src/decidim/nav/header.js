6accbfe85b02dcf8f1bc8fc967384402a919575a
Joonas <joonas.aapro@mainiotech.fi>
2025-04-24 09:20:02 +0300

    Add mobile changes

--- app/packs/src/decidim/nav/header.js
+++ app/packs/src/decidim/nav/header.js
@@ -1,4 +1,5 @@
 const activeLink = () => {
+  // For desktop
   const links = document.querySelectorAll("#menu-bar a");
 
   links.forEach(link => {
@@ -8,24 +9,66 @@ const activeLink = () => {
       link.classList.add("active-link");
     }
   });
+
+  // For mobile
+  const mobileLinks = document.querySelectorAll("#mobile-menu a");
+
+  mobileLinks.forEach(link => {
+    link.classList.add("menu-link");
+
+    if (window.location.pathname.includes(link.getAttribute("href"))) {
+      link.classList.add("active-link");
+    }
+  })
 }
 
 const activeLocale = () => {
-  const localesContainer = document.querySelector(".simple-locales-container");
+  const localesContainers = document.querySelectorAll(".simple-locales-container");
 
-  if (localesContainer) {
-    const locales = localesContainer.querySelectorAll(".locale");
-    const docLanguage = document.documentElement.lang;
+  localesContainers.forEach(localesContainer => {
+    if (localesContainer) {
+      const locales = localesContainer.querySelectorAll(".locale");
+      const docLanguage = document.documentElement.lang;
 
-    locales.forEach(locale => {
-      if (docLanguage === locale.getAttribute("lang")) {
-        locale.classList.add("active-locale");
+      locales.forEach(locale => {
+        if (docLanguage === locale.getAttribute("lang")) {
+          locale.classList.add("active-locale");
+        }
+      })
+    }
+  })
+}
+
+const initializeMobileMode = () => {
+  const mobileMenuButton = document.getElementById("toggle-mobile-menu");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  mobileMenuButton.addEventListener("click", () => {
+    const isHidden = mobileMenu.classList.toggle("hidden");
+
+    document.body.classList.toggle("overflow-hidden", !isHidden);
+  })
+
+  document.querySelectorAll("[data-toggle-target]").forEach(caret => {
+    caret.addEventListener("click", (e) => {
+      e.stopPropagation();
+      caret.classList.toggle("rotate-180")
+      const targetId = caret.getAttribute("data-toggle-target");
+      const submenu = document.getElementById(targetId);
+
+      if (submenu) {
+        if (submenu.style.display === "block") {
+          submenu.style.display = "none";
+        } else {
+          submenu.style.display = "block";
+        }
       }
-    })
-  }
+    });
+  });
 }
 
 $(() => {
   activeLink();
   activeLocale();
+  initializeMobileMode();
 })
