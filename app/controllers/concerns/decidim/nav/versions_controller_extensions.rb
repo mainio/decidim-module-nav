6789ec6efe988e7ac1a329edec184f5af72bd0b9
Joonas <joonas.aapro@mainiotech.fi>
2026-03-24 16:02:50 +0200

    Add breadcrumbs for records' version -view
    In version views the breadcrumbs 'wiped' the record's breadcrumb and version page didn't have one. This change keeps the breadcrumb for the record which serves as a link to go back to the record's page. The version page doesn't display a breadcrumb itself

---
+++ app/controllers/concerns/decidim/nav/versions_controller_extensions.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Nav
+    module VersionsControllerExtensions
+      extend ActiveSupport::Concern
+
+      included do
+        include Decidim::TranslatableAttributes
+
+        before_action :set_controller_breadcrumb
+
+        def controller_breadcrumb_items
+          @controller_breadcrumb_items ||= []
+        end
+
+        def set_controller_breadcrumb
+          controller_breadcrumb_items.concat(breadcrumb_item)
+        end
+
+        def breadcrumb_item
+          return [] unless versioned_resource
+
+          items = []
+
+          if versioned_resource.is_a?(Decidim::Accountability::Result) && versioned_resource&.parent.present?
+            items << {
+              label: translated_attribute(versioned_resource.parent.title),
+              url: versioned_resource_path(versioned_resource.parent),
+              active: true
+            }
+          end
+
+          items << {
+            label: translated_attribute(versioned_resource.title),
+            url: versioned_resource_path(versioned_resource),
+            active: true
+          }
+        end
+
+        def versioned_resource_path(versioned_resource)
+          case versioned_resource
+          when Decidim::Proposals::ProposalPresenter
+            proposal_path(versioned_resource)
+          when Decidim::Accountability::Result
+            result_path(versioned_resource)
+          when Decidim::Meetings::Meeting
+            meeting_path(versioned_resource)
+          when Decidim::Debates::DebatePresenter
+            debate_path(versioned_resource)
+          end
+        end
+      end
+    end
+  end
+end
