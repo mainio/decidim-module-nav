56eefc3dabc80eba7e4c5d823cc0db580b549ad9
Joonas <joonas.aapro@mainiotech.fi>
2026-03-27 14:50:49 +0200

    Add breadcrumbs to devise views and pages
    (cherry picked from commit 3f66e1c494b67892535f5e52ba13263114af8ef8)

---
+++ app/controllers/concerns/decidim/nav/unlocks_controller_extensions.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Nav
+    module UnlocksControllerExtensions
+      extend ActiveSupport::Concern
+
+      included do
+        before_action :set_controller_breadcrumb
+
+        def controller_breadcrumb_items
+          @controller_breadcrumb_items ||= []
+        end
+
+        def set_controller_breadcrumb
+          controller_breadcrumb_items << breadcrumb_item
+        end
+
+        def breadcrumb_item
+          {
+            label: t("devise.unlocks.new.resend_unlock_instructions"),
+            url: request.path,
+            active: true
+          }
+        end
+      end
+    end
+  end
+end
